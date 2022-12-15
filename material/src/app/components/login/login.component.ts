1f51d4756fc9d53a51133a7ca8959b1becbd869d
Pablo Kohls <kohlsalejandro1999@gmail.com>
2022-12-15 07:40:29 -0500

    empezando angular login usuario y contraseña

---
+++ material/src/app/components/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  form: FormGroup
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      usuario: [ , Validators.required],
+      password: [, Validators.required]
+    })
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  ingresar() {
+    console.log(this.form);
+  }
+
+}
